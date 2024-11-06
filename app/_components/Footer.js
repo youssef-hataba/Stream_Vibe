882c45292769cc6d461d22b7931028d41923f083
rawan ayman <rawanrooney229@gmail.com>
2024-11-06 19:42:37 +0200

    mobie details

--- app/_components/Footer.js
+++ app/_components/Footer.js
@@ -65,7 +65,7 @@ export default function Footer() {
             <h3 className="text-lg font-semibold mb-4">Support</h3>
             <ul className="space-y-2">
               <li>
-                <Link href="/" className="hover:underline text-gray-400">
+                <Link href="/support" className="hover:underline text-gray-400">
                   Contact Us
                 </Link>
               </li>
